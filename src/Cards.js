5313792df089a0a8a9d9c3d60ac942e5edc9df84
carmenddi <carmendarca@gmail.com>
2025-02-08 20:04:43 +0100

    Update Cards.js

--- src/Cards.js
+++ src/Cards.js
@@ -1,4 +1,4 @@
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import Owner from './images/owner.png';
 import Styling from './images/styling.jpeg';
